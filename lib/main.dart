50305e4de04d0f5d9be99e6cc66de856d7345ced
Muneeb Ahmed <muneebahmedf19@nutech.edu.pk>
2025-08-04 22:59:59 +0500

    Background Notifications will now be handled

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:safezone/UI/Splash%20Screen.dart';
+import 'package:safezone/global%20variables.dart';
+import 'package:safezone/services/notificationServices.dart';
 
+Future<void> backgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  FirebaseMessaging.instance.onTokenRefresh.listen((event) {
+    print(event.toString());
+    Token = event.toString();
+  });
+  NotificationServices().firebaseInit();
+}
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
